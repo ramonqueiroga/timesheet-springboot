8aa3489dfad2c02f68597d5cd7ccdb03a0ef108f
ramonqueiroga <ramon.araujo.queiroga@gmail.com>
2016-06-25 10:38:39 -0300

    Adicionado um novo bean para criação do CorsFilter. Esse bean resolve o problema de "No 'Access-Control-Allow-Origin' header is present on the requested resource" que acontecia quando era feita uma requisição do frontend a algum recurso do backend via rest

---
+++ src/main/java/br/com/timesheet/configuration/rest/RestConfiguration.java
@@ -0,0 +1,31 @@
+package br.com.timesheet.configuration.rest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by ramon on 25/06/16.
+ */
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
